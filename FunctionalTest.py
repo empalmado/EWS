aabcebd4239fed61637977d8a34544cbe8340778
Drin <drin@drin.lan>
2021-04-06 15:43:09 +0800

    New FunctionalTest with unittest module

--- FunctionalTest.py
+++ FunctionalTest.py
@@ -1,3 +1,20 @@
 from selenium import webdriver
-browser = webdriver.Firefox()
-browser.get('http://127.0.0.1:8000')
+import unittest
+#browser = webdriver.Firefox()#
+#browser.get('http://127.0.0.1:8000')#
+
+class ProjectDrin(unittest.TestCase):
+
+	def setup(self):
+	   self.browser = webdriver.firefox()
+	   
+	def tearDown(self):
+	   self.browser.quit()
+	   
+	def test_browser_title(self):
+	    self.browser.get('http://localhost:8000')
+	    self.assertIn('Project Drin', self.browser.title)
+	    self.fail('Finish the test NOW!!!???')
+	    
+if __name__=='__main__':
+	unittest.main()
