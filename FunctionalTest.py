7248711b305df81a7a31e62b075fef911e37333c
aldrin <aldrin.empalmado@gsfe.tupcavite.edu.ph>
2021-04-26 05:21:08 +0800

    add

--- FunctionalTest.py
+++ FunctionalTest.py
@@ -1,5 +1,94 @@
 from selenium import webdriver
-browser = webdriver.Firefox()
-browser.get('http:/127.0.0.1:8000')
+import unittest
+
+from selenium.webdriver.common.keys import Keys
+import time
+
+class PageTest(unittest.TestCase):
+
+	
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+		
+
+	def test_browser_title(self):
+		self.browser.get('http://localhost:8000')
+		self.assertIn('Empalmado Welding Shop', self.browser.title)
+		
+		hder = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('EWS Free Estimate', hder)
+		
+		l1 = self.browser.find_element_by_id('Name').text
+		self.assertIn('Name or Company Name:', l1)
+
+		l2 = self.browser.find_element_by_id('Contact').text
+		self.assertIn('Contact No.:', l2)
+
+		l3 = self.browser.find_element_by_id('Iname').text
+		self.assertIn('Product Name:', l3)
+
+		l4 = self.browser.find_element_by_id('Size').text
+		self.assertIn('Sizes of Product in inch/ft:', l4)
+
+		l5 = self.browser.find_element_by_id('Type').text
+		self.assertIn('Stainless Type 304 or 202:', l5)
+
+		in1 = self.browser.find_element_by_id('Names')  
+		in1 = self.browser.find_element_by_id('Names').send_keys("Aldrin Empalmado")
+		time.sleep(2)
+
+		in2 = self.browser.find_element_by_id('Contacts')  
+		in2 = self.browser.find_element_by_id('Contacts').send_keys("09092116086")
+		time.sleep(2)
+
+		in3 = self.browser.find_element_by_id('Inames')  
+		in3 = self.browser.find_element_by_id('Inames').send_keys("Griller")
+		time.sleep(2)
+
+		in4 = self.browser.find_element_by_id('Sizes')  
+		in4 = self.browser.find_element_by_id('Sizes').send_keys("12'x24'")
+		time.sleep(2)
+
+		in5 = self.browser.find_element_by_id('Types')  
+		in5 = self.browser.find_element_by_id('Types').send_keys("304")
+		time.sleep(2)
+
+		submitbutton = self.browser.find_element_by_name('Submit').click()
+		time.sleep(2)
 
 
+		self.browser.quit()	
+		self.browser = webdriver.Firefox()
+		self.browser.get('http://localhost:8000')
+		
+		in1 = self.browser.find_element_by_id('Names')  
+		in1 = self.browser.find_element_by_id('Names').send_keys("Aldrin Empalmado")
+		time.sleep(2)
+
+		in2 = self.browser.find_element_by_id('Contacts')  
+		in2 = self.browser.find_element_by_id('Contacts').send_keys("09092116086")
+		time.sleep(2)
+
+		in3 = self.browser.find_element_by_id('Inames')  
+		in3 = self.browser.find_element_by_id('Inames').send_keys("Griller")
+		time.sleep(2)
+
+		in4 = self.browser.find_element_by_id('Sizes')  
+		in4 = self.browser.find_element_by_id('Sizes').send_keys("12'x24'")
+		time.sleep(2)
+
+		in5 = self.browser.find_element_by_id('Types')  
+		in5 = self.browser.find_element_by_id('Types').send_keys("304")
+		time.sleep(2)
+
+		submitbutton = self.browser.find_element_by_name('Submit').click()
+		time.sleep(2)
+
+
+		self.browser.quit()	
+		self.browser = webdriver.Firefox()
+		self.browser.get('http://localhost:8000')
+
+
+if __name__ == '__main__':
+	unittest.main()
